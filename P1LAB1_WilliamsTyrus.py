157a34f3751d808cc68e99b3540e488c67dd482e
T-Will-do-it <williamt8851@student.faytechcc.edu>
2024-09-22 21:07:36 -0400

    Add files via upload

---
+++ P1LAB1_WilliamsTyrus.py
@@ -0,0 +1,7 @@
+#Tyrus Williams
+#09/22/2024
+#P1LAB1
+#Welcome students to class using python
+first_name = input('Enter your first name:')
+last_name = input('Enter your last name:')
+print('Hello,', first_name, last_name, '! Welcome to CTI-110')
